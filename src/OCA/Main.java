5a7477938838c12f489da9d4d9631dd646de3de7
drumtheart <drumtheart@gmail.com>
2019-09-18 23:20:07 +0200

    BuilderPattern

--- src/OCA/Main.java
+++ src/OCA/Main.java
@@ -1,4 +1,11 @@
 package OCA;
 
 public class Main {
+    public static void main(String[] args) {
+
+        int x1 = 50, x2 = 75;
+        boolean b = x1 >= x2;
+5: if(b = true) System.out.println("Success");
+6: else System.out.println("Failure");
+}
 }
